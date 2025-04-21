48e71250f45fe361d0648a1ad412a201967e19a1
Luke Stano <147096339+lukestano22@users.noreply.github.com>
2025-04-21 00:07:52 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+package module_1;
+
+public class main {
+    public static void main(String[] args) {
+        String firstName = "Rudy";
+        String lastName = "Gillespie";
+        String address = "123 Appleberry ln";
+        String city = "Seaford";
+        int zip = 11223;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(address);
+        System.out.println(city);
+        System.out.println(zip);
+
+    }
+}
